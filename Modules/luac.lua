6b969b6af7d704933f57f9a776139ed90768bf14
xmancheng <104349780+xmancheng@users.noreply.github.com>
2023-04-01 21:59:05 +0800

    Update luac.lua

--- Modules/luac.lua
+++ Modules/luac.lua
@@ -20,8 +20,6 @@ function Luac:yboffline(player)--玩家下线清空自己召唤的ai
 	wanjia[player][0] = 0
 end
 
-local hbAi = {"特殊小伙伴","物理小伙伴","变化小伙伴","干扰小伙伴","法术小伙伴"}
-
 local hbsp = {900330,900333}--饰品
 
 local mfsj = {
@@ -77,7 +75,7 @@ function Luac:onLoad()
 					Char.SetData(charIndex1, CONST.CHAR_X, Char.GetData(player, CONST.CHAR_X));
 					Char.SetData(charIndex1, CONST.CHAR_Y, Char.GetData(player, CONST.CHAR_Y));
 					Char.SetData(charIndex1, CONST.CHAR_地图, Char.GetData(player, CONST.CHAR_地图));
-					Char.SetData(charIndex1, CONST.CHAR_名字, hbAi[math.random(1,5)]);
+					Char.SetData(charIndex1, CONST.CHAR_名字, Char.GetData(petIndex1,CONST.CHAR_名字));
 					Char.SetData(charIndex1, CONST.CHAR_地图类型, Char.GetData(player,CONST.CHAR_地图类型));
 					Char.SetData(charIndex1, CONST.CHAR_形象, Char.GetData(petIndex1,CONST.CHAR_形象));
 					Char.SetData(charIndex1, CONST.CHAR_原形, Char.GetData(petIndex1,CONST.CHAR_原形));
@@ -119,7 +117,9 @@ function Luac:onLoad()
 					Char.MoveItem(charIndex1, 9, CONST.EQUIP_首饰1, -1)
 					Char.MoveItem(charIndex1, 10, CONST.EQUIP_首饰2, -1)
 					Char.GiveItem(charIndex1, 18196, 1);--实验药
-					Char.GiveItem(charIndex1, 18315, 19);--火把
+					Char.GiveItem(charIndex1, 18315, 1);--火把
+					Char.GiveItem(charIndex1, 900201, 1);--攻击《基本》卡
+					Char.GiveItem(charIndex1, 900202, 1);--明镜《基本》卡
 
 					Char.SetData(charIndex1, CONST.CHAR_血, Char.GetData(petIndex1,CONST.CHAR_最大血));
 					Char.SetData(charIndex1, CONST.CHAR_魔, Char.GetData(petIndex1,CONST.CHAR_最大魔));
