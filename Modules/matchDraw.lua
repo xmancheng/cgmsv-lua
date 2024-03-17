ab084e38eaef736860787655ac5aa9ee823bd346
xmancheng <104349780+xmancheng@users.noreply.github.com>
2024-03-17 22:42:50 +0800

    Update matchDraw.lua
    回復原本的語法才正確

--- Modules/matchDraw.lua
+++ Modules/matchDraw.lua
@@ -283,7 +283,7 @@ function MatchDraw:onMatchDraw(player, targetcharIndex, itemSlot)
           local WinNum = NLG.Rand(7, 1000);
           print(WinNum)
           local cdk = Char.GetData(player,CONST.对象_CDK);
-          SQL.Run("INSERT INTO lua_hook_character (Name,CdKey,OriginalImageNumber) SELECT Name,CdKey,OriginalImageNumber FROM tbl_character WHERE CdKey='"..cdk.."' WHERE NOT EXISTS ( SELECT Name FROM lua_hook_character WHERE CdKey='"..cdk.."')");
+          SQL.Run("INSERT INTO lua_hook_character (Name,CdKey,OriginalImageNumber) SELECT Name,CdKey,OriginalImageNumber FROM tbl_character WHERE NOT EXISTS ( SELECT Name FROM lua_hook_character WHERE CdKey='"..cdk.."')");
           local tenjo = tonumber(SQL.Run("select Tenjo from lua_hook_character where CdKey='"..cdk.."'")["0_0"])
           for k, v in ipairs(DrawTbl) do
              if (WinNum>=v.serial_L and WinNum<=v.serial_H) then
