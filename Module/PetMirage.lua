a7343f2b1d43831adf5b9dd02051c01dda9ca7da
xmancheng <104349780+xmancheng@users.noreply.github.com>
2024-04-29 00:36:01 +0800

    Update PetMirage.lua
    繁體化

--- Module/PetMirage.lua
+++ Module/PetMirage.lua
@@ -33,11 +33,11 @@ function PetMirage_create()
 		PetMirageNPC = NL.CreateNpc("lua/Module/PetMirage.lua", "initPetMirageNpc_Init");
 		Char.SetData(PetMirageNPC,%对象_形象%,104893);
 		Char.SetData(PetMirageNPC,%对象_原形%,104893);
-		Char.SetData(PetMirageNPC,%对象_地图%,1000);
-		Char.SetData(PetMirageNPC,%对象_X%,232);
-		Char.SetData(PetMirageNPC,%对象_Y%,83);
-		Char.SetData(PetMirageNPC,%对象_方向%,4);
-		Char.SetData(PetMirageNPC,%对象_名字%,"通灵人");
+		Char.SetData(PetMirageNPC,%对象_地图%,1180);
+		Char.SetData(PetMirageNPC,%对象_X%,10);
+		Char.SetData(PetMirageNPC,%对象_Y%,15);
+		Char.SetData(PetMirageNPC,%对象_方向%,6);
+		Char.SetData(PetMirageNPC,%对象_名字%,"通靈人");
 		NLG.UpChar(PetMirageNPC);
 		Char.SetTalkedEvent("lua/Module/PetMirage.lua", "PetMirageWindow", PetMirageNPC);
 		Char.SetWindowTalkedEvent("lua/Module/PetMirage.lua", "PetMirageFunction", PetMirageNPC);
@@ -46,7 +46,7 @@ end
 
 function PetMirageWindow(_NpcIndex,_PlayerIndex)
 	if (NLG.CanTalk(_NpcIndex,_PlayerIndex) == true) then
-		WindowMsg = "3|\\n\\n↓选择制作成灵体的宠物(回收并提取形象)↓ \\n\\n";
+		WindowMsg = "3|\\n\\n↓選擇製作成靈體的寵物(回收並提取形象)↓ \\n\\n";
 		for i=0,4 do
 			local pet = Char.GetPet(_PlayerIndex,i);
 	
@@ -64,15 +64,15 @@ end
 NL.RegItemString("lua/Module/PetMirage.lua","TransPet","LUA_useTransPet");
 function TransPet(_PlayerIndex,_toIndex,_itemslot) --双击道具执行函数
 	ItemID = Item.GetData(Char.GetItemIndex(_PlayerIndex,_itemslot),0);
-	local TalkMsg =		"\\n                ◆宠物超灵体◆" ..
-				"\\n将宠物形象和属性附身至水晶" ..
-				"\\n并使角色外观改变为宠物形象" ..
+	local TalkMsg =		"\\n                ◆寵物超靈體◆" ..
+				"\\n將寵物形象和屬性附身至水晶" ..
+				"\\n並使角色外觀改變為寵物形象" ..
 				"\\n " ..
-				"\\n卸下水晶即解除超灵体状态" ..
-				"\\n装备水晶即回复超灵体状态" ..
+				"\\n卸下水晶即解除超靈體狀態" ..
+				"\\n裝備水晶即回復超靈體狀態" ..
 				"\\n " ..
-				"\\n选择    『依附在水晶』"..
-				"\\n " 
+				"\\n選擇    『依附在水晶』"..
+				"\\n "
 	NLG.ShowWindowTalked(_PlayerIndex, PetMirageNPC,%窗口_信息框%,%按钮_是否%, 2, TalkMsg);
 	return 1;
 end
@@ -83,13 +83,13 @@ function PetMirageFunction(_NpcIndex,_PlayerIndex,_SqeNo,_select,_data)
 		local selectitem = tonumber(_data) - 1;
 		print(tonumber(_data));
 		if(selectitem == nil or selectitem > 4 or selectitem < 0) then
-			NLG.SystemMessage(_PlayerIndex,"[系统]您所选择的位置不正常!");
+			--NLG.SystemMessage(_PlayerIndex,"[系统]您所选择的位置不正常!");
 			return;
 		end
 
 		local _PetIndex = Char.GetPet(_PlayerIndex,selectitem);
 		if (VaildChar(_PetIndex) == false) then
-			NLG.SystemMessage(_PlayerIndex,"[系统]请确定您对应的宠物栏有宠物!");
+			NLG.SystemMessage(_PlayerIndex,"[系統]請確定您對應的寵物欄有寵物!");
 			return;
 		end
 --		if(Char.GetData(_PetIndex,%对象_等级%) ~= 1) then
@@ -120,11 +120,11 @@ function PetMirageFunction(_NpcIndex,_PlayerIndex,_SqeNo,_select,_data)
 		local Para2 = Item.GetData(item_indexB,%道具_子参二%);
 
 		if (Char.ItemNum(_PlayerIndex,itemid_card) > 0 and Char.ItemNum(_PlayerIndex,itemid_ball) > 0) then
-			NLG.SystemMessage(_PlayerIndex,"[系统]请确认物品栏只能有宠物变身卡或宠物附身媒介");
+			NLG.SystemMessage(_PlayerIndex,"[系統]請確認物品欄只能有寵物變身卡或寵物附身媒介");
 			return;
 		end
 		if (Char.ItemNum(_PlayerIndex,itemid_card) > 0 and Para2 == 0 and type == 14 and Gold < 10000) then
-			NLG.SystemMessage(_PlayerIndex,"[系统]请确认金钱尚有一万!");
+			NLG.SystemMessage(_PlayerIndex,"[系統]請確認金錢尚有一萬G!");
 			return;
 		end
 		if (Char.ItemNum(_PlayerIndex,itemid_card) > 0 and Para2 == 0 and type == 14 and Gold >= 10000 and Char.ItemNum(_PlayerIndex,itemid_ball) <=0 ) then
@@ -133,23 +133,23 @@ function PetMirageFunction(_NpcIndex,_PlayerIndex,_SqeNo,_select,_data)
 			Item.SetData(item_indexB,%道具_名字%,Newname);
 			Item.UpItem(_PlayerIndex,8);
 			Char.AddGold(_PlayerIndex,-10000);
-			Msg = "\\n\\n\\n     您的宠物 "..Char.GetData(_PetIndex,%对象_名字%).." 灵体附身在卡片了!   \\n"
+			Msg = "\\n\\n\\n     您的寵物 "..Char.GetData(_PetIndex,%对象_名字%).." 靈體附身在卡片了!   \\n"
 					.."  \\n"
-					.."            寻找帕契族附身合体\\n"
-					.."            将任意宠物形象改变\\n";
+					.."            尋找帕契族附身合體\\n"
+					.."            將任意寵物形象改變\\n";
 			NLG.ShowWindowTalked(_PlayerIndex,_NpcIndex,%窗口_信息框%,%按钮_确定%,3,Msg);
 			return 0;
 		end
 		if (Char.ItemNum(_PlayerIndex,itemid_card) > 0 and Para2 ~= 0 and type == 14) then
-			NLG.SystemMessage(_PlayerIndex,"[系统]请确认物品栏第一栏是否为未使用宠物变身卡！");
+			NLG.SystemMessage(_PlayerIndex,"[系統]請確認物品欄第一欄是否為未使用寵物變身卡！");
 			return;
 		end
 		if (Char.ItemNum(_PlayerIndex,itemid_card) >= 0 and type ~= 14) then
-			NLG.SystemMessage(_PlayerIndex,"[系统]请确认物品栏第一栏是否为宠物变身卡或宠物附身媒介！");
+			NLG.SystemMessage(_PlayerIndex,"[系統]請確認物品欄第一欄是否為寵物變身卡或寵物附身媒介！");
 			return;
 		end
 		if (Char.ItemNum(_PlayerIndex,itemid_ball) > 0 and Para2 == 0 and type == 14 and Gold < 100000) then
-			NLG.SystemMessage(_PlayerIndex,"[系统]请确认金钱尚有十万!");
+			NLG.SystemMessage(_PlayerIndex,"[系統]請確認金錢尚有十萬G!");
 			return;
 		end
 		if (Char.ItemNum(_PlayerIndex,itemid_ball) > 0 and Para2 == 0 and type == 14 and Gold >= 100000 and Char.ItemNum(_PlayerIndex,itemid_card) <=0 ) then
@@ -185,21 +185,21 @@ function PetMirageFunction(_NpcIndex,_PlayerIndex,_SqeNo,_select,_data)
 			Item.SetData(item_indexB,%道具_属性二值%,Ameter_2);
 			Item.UpItem(_PlayerIndex,8);
 			Char.AddGold(_PlayerIndex,-100000);
-			Msg = "\\n\\n\\n     您的宠物 "..Char.GetData(_PetIndex,%对象_名字%).." 灵体附身在媒介了!   \\n"
-					.."            请双击宠物附身媒介\\n"
-					.."            让水晶可以宠物变身\\n"
+			Msg = "\\n\\n\\n     您的寵物 "..Char.GetData(_PetIndex,%对象_名字%).." 灵体附身在媒介了!   \\n"
+					.."            請按兩下寵物附身媒介\\n"
+					.."            讓水晶可以寵物變身\\n"
 					.."  \\n"
-					.."            人物装备时超灵体状态\\n"
-					.."            形象变身成该宠物外观\\n";
+					.."            人物裝備時超靈體狀態\\n"
+					.."            形象變身成該寵物外觀\\n";
 			NLG.ShowWindowTalked(_PlayerIndex,_NpcIndex,%窗口_信息框%,%按钮_确定%,3,Msg);
 			return 0;
 		end
 		if (Char.ItemNum(_PlayerIndex,itemid_ball) > 0 and Para2 ~= 0 and type == 14) then
-			NLG.SystemMessage(_PlayerIndex,"[系统]请确认物品栏第一栏是否为未使用宠物附身媒介！");
+			NLG.SystemMessage(_PlayerIndex,"[系統]請確認物品欄第一欄是否為未使用寵物附身媒介！");
 			return;
 		end
 		if (Char.ItemNum(_PlayerIndex,itemid_ball) >= 0 and type ~= 14) then
-			NLG.SystemMessage(_PlayerIndex,"[系统]请确认物品栏第一栏是否为宠物变身卡或宠物附身媒介！");
+			NLG.SystemMessage(_PlayerIndex,"[系統]請確認物品欄第一欄是否為寵物變身卡或寵物附身媒介！");
 			return;
 		end
 	end
@@ -213,15 +213,15 @@ function PetMirageFunction(_NpcIndex,_PlayerIndex,_SqeNo,_select,_data)
 			return
 		end
 		if (_select==4 and Item.GetData(item_indexB,%道具_子参二%)==0) then
-			NLG.SystemMessage(_PlayerIndex,"[系统]请确认宠物灵体是否附身在媒介了！");
+			NLG.SystemMessage(_PlayerIndex,"[系統]請確認寵物靈體是否附身在媒介了！");
 			return;
 		end
 		if (nameC == nill) then
-			NLG.SystemMessage(_PlayerIndex,"[系统]请确定您的装备栏有水晶!");
+			NLG.SystemMessage(_PlayerIndex,"[系統]請確定您的裝備欄有水晶!");
 			return;
 		end
 		if(string.find(nameC,"in") ~= nill ) then
-			NLG.SystemMessage(_PlayerIndex, "[系统]水晶无法再依附宠物灵体。");
+			NLG.SystemMessage(_PlayerIndex, "[系統]水晶無法再依附寵物靈體。");
 			return;
 		end
 		if (_select==4 and Item.GetData(item_indexB,%道具_子参二%)~=0 and OverSoulEnable[TargetItemID] ~= 0) then
@@ -271,9 +271,9 @@ function PetMirageFunction(_NpcIndex,_PlayerIndex,_SqeNo,_select,_data)
 			Item.SetData(item_indexC,58,itemdirect);
 			Item.UpItem(_PlayerIndex,7);
 			Char.DelItem(_PlayerIndex,ItemID,1);
-			NLG.SystemMessage(_PlayerIndex,"超灵体状态OVER-SOUL！");
+			NLG.SystemMessage(_PlayerIndex,"超靈體狀態OVER-SOUL！");
 		else
-			NLG.SystemMessage(_PlayerIndex,"[系统]无法超灵体依附的水晶！");
+			NLG.SystemMessage(_PlayerIndex,"[系統]無法超靈體依附的水晶！");
 		end
 	end
 end
