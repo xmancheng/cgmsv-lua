b0034912b311995edaec3ea6b2235ef51bbd5c3a
xmancheng <104349780+xmancheng@users.noreply.github.com>
2024-05-07 23:47:36 +0800

    Update setupItemType.lua

--- Modules/setupItemType.lua
+++ Modules/setupItemType.lua
@@ -10,7 +10,7 @@ function Module:onLoad()
     { type=67, name="陷阱", defaultImage=27938, place=3, flags=0, job=2002, level=10 },
     { type=68, name="書籍", defaultImage=27446, place=3, flags=0, job=2003, level=10 },
     { type=69, name="風魔", defaultImage=99521, place=3, flags=0, job=2004, level=10 },
-    { type=70, name="拳套", defaultImage=21303, place=3, flags=0, job=2005, level=10 },
+    { type=70, name="拳套", defaultImage=21303, place=2, flags=128, job=2005, level=10 },
   }
   for k, v in ipairs(itemType) do
       Item.CreateNewItemType(v.type, v.name, v.defaultImage, v.place, v.flags)
@@ -20,6 +20,20 @@ function Module:onLoad()
   end
   --self:regCallback('ItemAttachEvent', Func.bind(self.ItemAttachEvent, self))
   --self:regCallback('ItemDetachEvent', Func.bind(self.ItemDetachEvent, self))
+
+  --宠物装
+  local petEquipType = {
+    { type=56, name="宠物水晶", place=CONST.宠道栏_水晶, enable=1 },
+    { type=57, name="宠物饰品", place=CONST.宠道栏_饰品1, enable=1 },
+    { type=58, name="宠物装甲", place=CONST.宠道栏_身替, enable=1 },
+    { type=59, name="宠物服饰", place=CONST.宠道栏_身替, enable=1 },
+    { type=60, name="宠物颈圈", place=CONST.宠道栏_颈圈, enable=1 },
+    { type=61, name="宠物护符", place=CONST.宠道栏_饰品2, enable=1 },
+  }
+  --for k, v in ipairs(petEquipType) do
+  --    Item.SetPetEquipPlace(v.type, v.place, v.enable)
+  --end
+
 end
 
 --- 卸载模块钩子
