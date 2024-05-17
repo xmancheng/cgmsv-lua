15c2f002fc9db8c0b383197a4562fe9c8ddb0e8e
xmancheng <104349780+xmancheng@users.noreply.github.com>
2024-05-17 15:36:45 +0800

    Update StrengthShop.lua
    商品格式備份

--- Modules/StrengthShop.lua
+++ Modules/StrengthShop.lua
@@ -1,31 +1,46 @@
 local Module = ModuleBase:createModule('StrengthShop');
 
 local itemFields={
-CONST.道具_类型,
-CONST.道具_堆叠数,
-CONST.道具_等级,
-CONST.道具_耐久,
-CONST.道具_最大耐久,
-CONST.道具_攻击,
-CONST.道具_防御,
-CONST.道具_敏捷,
-CONST.道具_精神,
-CONST.道具_回复,
-CONST.道具_必杀,
-CONST.道具_反击,
-CONST.道具_命中,
-CONST.道具_闪躲,
-CONST.道具_生命,
-CONST.道具_魔力,
-CONST.道具_毒抗,
-CONST.道具_睡抗,
-CONST.道具_石抗,
-CONST.道具_醉抗,
-CONST.道具_乱抗,
-CONST.道具_忘抗,
-CONST.道具_魔抗,
-CONST.道具_魔攻,
+{CONST.道具_攻击,CONST.ITEMSET_MODIFYATTACK,"攻擊"},
+{CONST.道具_防御,CONST.ITEMSET_MODIFYDEFENCE,"防禦"},
+{CONST.道具_敏捷,CONST.ITEMSET_MODIFYAGILITY,"敏捷"},
+{CONST.道具_精神,CONST.ITEMSET_MODIFYMAGIC,"精神"},
+{CONST.道具_回复,CONST.ITEMSET_MODIFYRECOVERY,"回復"},
+{CONST.道具_必杀,CONST.ITEMSET_MODIFYCRITICAL,"必殺"},
+{CONST.道具_反击,CONST.ITEMSET_MODIFYCOUNTER,"反擊"},
+{CONST.道具_命中,CONST.ITEMSET_MODIFYHITRATE,"命中"},
+{CONST.道具_闪躲,CONST.ITEMSET_MODIFYAVOID,"閃躲"},
+{CONST.道具_生命,CONST.ITEMSET_MODIFYHP,"生命"},
+{CONST.道具_魔力,CONST.ITEMSET_MODIFYFORCEPOINT,"魔力"},
+{CONST.道具_毒抗,CONST.ITEMSET_POISON,"抗毒"},
+{CONST.道具_睡抗,CONST.ITEMSET_SLEEP,"抗睡"},
+{CONST.道具_石抗,CONST.ITEMSET_STONE,"抗石"},
+{CONST.道具_醉抗,CONST.ITEMSET_DRUNK,"抗罪"},
+{CONST.道具_乱抗,CONST.ITEMSET_CONFUSION,"抗亂"},
+{CONST.道具_忘抗,CONST.ITEMSET_AMNESIA,"抗忘"},
+{CONST.道具_等级,CONST.ITEMSET_LEVEL,"等級"},
+{CONST.道具_耐久,CONST.ITEMSET_REMAIN,"耐久"},
+{CONST.道具_最大耐久,CONST.ITEMSET_MAXREMAIN,"最大耐久"},
+{CONST.道具_类型,CONST.ITEMSET_TYPE,"其他卡片"},
+{CONST.道具_子参一,CONST.ITEMSET_SPECIALEFFECTVALUE,"SPECIALEFFECTVALUE"},
+{CONST.道具_堆叠数,CONST.ITEMSET_COST,"數量"},
 }
+
+function WhichPart(Para1)
+    local WhichPart={}
+    if Para1==0 then            --头
+        WhichPart={"               $5賦予頭部"};
+    elseif Para1==1 then    --身
+        WhichPart={"               $5賦予身體"};
+    elseif Para1==3 then    --右
+        WhichPart={"               $5賦予副武"};
+    elseif Para1==4 then    --足
+        WhichPart={"               $5賦予足部"};
+    elseif Para1==5 then    --饰
+        WhichPart={"               $5賦予飾品"};
+    end
+    return WhichPart;
+end
 
 function Module:onLoad()
   self:logInfo('load')
@@ -58,18 +73,45 @@ function Module:onLoad()
         else
                itemMenu={};
         end
+        itemList={};
         -- 回调 data = 0:物品序号(基于本次交易)|N:购买数量|物品2序号|物品2数量...
         -- 数据结构 NPC图档|窗口标题|NPC对话|钱不够对话|拿不下或数量不够对话|物品N名称|物品N图档|物品N价格|物品N介绍|物品N类型|可否购买, 0不可, 1可|
         local windowStr = '231050|卷軸倉庫|準備取出卷軸…\n這些是你曾經存入的\n各式各樣的卷軸哦|\n你錢不夠|\n你拿不下了|';
-        for itemSlot=1, 20 do
-            local ItemIndex = Char.GetItemIndex(player,itemSlot);
-            if (ItemIndex>0) then
-                 local ItemName = Item.GetData(ItemIndex, CONST.道具_名字);
-                 local ItemImage = Item.GetData(ItemIndex, CONST.道具_图);
-                 local ItemType = Item.GetData(ItemIndex, CONST.道具_类型);
-                 local ItemID = Item.GetData(ItemIndex,CONST.道具_ID);
-                 local itemInfo = self:extractItemData(ItemIndex);
-                 windowStr = windowStr..''..ItemName..'|'..ItemImage..'|0|$5物品'..itemSlot..'介绍|'..ItemType..'|1|';
+        for im=1,#itemMenu do
+            local ItemsetIndex = Data.ItemsetGetIndex(itemMenu[im][2]);
+            if (ItemsetIndex>0) then
+                 --local ItemName = Item.GetData(ItemIndex, CONST.道具_名字);
+                 local ItemName = Data.ItemsetGetData(ItemsetIndex, CONST.ITEMSET_TRUENAME);
+                 --local ItemImage = Item.GetData(ItemIndex, CONST.道具_图);
+                 local ItemImage = Data.ItemsetGetData(ItemsetIndex, CONST.ITEMSET_BASEIMAGENUMBER);
+                 local ItemInfo = self:extractItemData(ItemsetIndex);
+                 local ItemMsg = "$1";
+                 for k,v in pairs(itemFields) do
+                     if ItemInfo.attr[tostring(v[1])] ~=nil  then
+                         local param = v[3];
+                         --print(param)
+                         if param=="耐久" then
+                             ItemMsg = ItemMsg .. "\\n$4"..param .." 0100"
+                         elseif param=="最大耐久" then
+                             ItemMsg = ItemMsg .. "/0100"
+                         elseif param=="等級" then
+                             ItemMsg = ItemMsg .. "　$4等級".. ItemInfo.attr[tostring(v[1])] ..""
+                         elseif param=="其他卡片" then
+                             ItemMsg = ItemMsg .. " $0種類 其他卡片"
+                         elseif param=="SPECIALEFFECTVALUE" then
+                             partMsg = WhichPart(ItemInfo.attr[tostring(v[1])]);
+                             ItemMsg = ItemMsg .. "\\n".. partMsg[1] ..""
+                         elseif param=="數量" then
+                             ItemMsg = ItemMsg .. "\\n\\n倉庫剩餘數量: ".. itemMenu[im][3] ..""
+                         else
+                             if (ItemInfo.attr[tostring(v[1])]~=0) then
+                                 ItemMsg = ItemMsg .. " ".. param .."+".. ItemInfo.attr[tostring(v[1])] .." "
+                             end
+                         end
+                     end
+                 end
+                 windowStr = windowStr..''..ItemName..'|'..ItemImage..'|0|'..ItemMsg..'|41|1|';
+                 itemList[im] = {name='..ItemName..', image='..ItemImage..', price=0, desc='..ItemMsg..', count=1, maxCount='..itemMenu[im][3]..', itemid=-1};
             end
         end
         NLG.ShowWindowTalked(player, StrengthShopNPC, CONST.窗口_买框, CONST.BUTTON_关闭, 11, windowStr);
@@ -89,9 +131,73 @@ function Module:onLoad()
          NLG.ShowWindowTalked(player, StrengthShopNPC, CONST.窗口_选择框, CONST.BUTTON_关闭, 12, winMsg);
      end
     end
+    if seqno == 11 then
+       if (select == CONST.按钮_关闭) then
+                 return;
+       end
+       local cdk = Char.GetData(player,CONST.对象_CDK);
+       local sqldata = SQL.Run("select ReelBag from lua_hook_character where CdKey='"..cdk.."'")["0_0"]
+       local itemData = {};
+       if (type(sqldata)=="string") then
+              itemData = JSON.decode(sqldata);
+              itemMenu = itemData;
+       else
+              itemMenu={};
+       end
+       local items = string.split(data, '|');
+       for k,v in ipairs(items) do
+           print(items[k])
+       end
+       for key = 1, #items / 2 do
+         local c = itemList[items[(key - 1) * 2 + 1] + 1]
+         if c then
+           count = (tonumber(items[(key - 1) * 2 + 2]) or 0);
+           if c.maxCount > 0 then
+             maxCount = c.maxCount - count;
+           else
+             maxCount = c.maxCount;
+             NLG.SystemMessage(player,"[系統]此物品倉庫已無存貨！");
+             return;
+           end
+           if c.itemid > 1 then
+             itemid = c.itemid;
+           else
+             itemid = -1;
+           end
+         end
+       end
+       if itemid == -1 then  ----排除透过按钮取得道具
+          return;
+       end
+       if maxCount < 0 then
+         NLG.SystemMessage(player,"[系統]數量超過庫存！");
+         return;
+       end
+       if (Char.ItemSlot(player)==20) then
+              maxCount = maxCount - count;
+              Char.GiveItem(player,itemid,count);
+       else
+              NLG.Say(player, -1, "注意提取數量超過物品欄！", CONST.颜色_黄色, CONST.字体_中);
+              return;
+       end
+       if (maxCount==0) then
+              table.remove(itemData,key);
+              itemMenu = itemData;
+       end
+       if (#itemMenu==0) then
+              itemMenu={};
+              SQL.Run("update lua_hook_character set ReelBag= NULL where CdKey='"..cdk.."'")
+              NLG.UpChar(player);
+              return;
+       else
+              local sqldata = itemData;
+              local newdata = JSON.encode(sqldata);
+              SQL.Run("update lua_hook_character set ReelBag= '"..newdata.."' where CdKey='"..cdk.."'")
+              NLG.UpChar(player);
+              return;
+       end
 
-
-
+    end
     if seqno == 12 then
        if (select == CONST.按钮_关闭) then
                  return;
@@ -116,7 +222,7 @@ function Module:onLoad()
               for k, v in pairs(itemData) do
                    if (itemData[k]~=nil and itemData[k][2]==itemid)  then
                          itemData[k][3] = itemData[k][3] + count;
-                   elseif (itemData[k][2]~=itemid) then
+                   elseif (itemData[k][1]~=itemid) then
                          boxCheck = boxCheck+1;
                    end
               end
@@ -148,12 +254,13 @@ function Module:onLoad()
 
 end
 
-function Module:extractItemData(ItemIndex)
+function Module:extractItemData(ItemsetIndex)
   local item = {
     attr={},
   };
   for _, v in pairs(itemFields) do
-    item.attr[tostring(v)] = Item.GetData(ItemIndex, v);
+    --item.attr[tostring(v[1])] = Item.GetData(ItemIndex, v[1]);
+    item.attr[tostring(v[1])] = Data.ItemsetGetData(ItemsetIndex, v[2]);
   end
   return item;
 end
