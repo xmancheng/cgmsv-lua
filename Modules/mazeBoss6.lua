8efe519b0eb83cf6739547dba76a79345978ba13
xmancheng <104349780+xmancheng@users.noreply.github.com>
2024-06-12 21:33:32 +0800

    Update mazeBoss6.lua

--- Modules/mazeBoss6.lua
+++ Modules/mazeBoss6.lua
@@ -43,8 +43,8 @@ local BossRoom = {
           lordName="暴走霸王",
        },    -- 超级(3)
 }
-tbl_duel_user = {};			--当前场次玩家的列表
-tbl_win_user = {};
+local tbl_duel_user = {};			--当前场次玩家的列表
+local tbl_win_user = {};
 ------------------------------------------------
 --- 加载模块钩子
 function Module:onLoad()
