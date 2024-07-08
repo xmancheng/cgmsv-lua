14fe814189a4c55df0f9575326a24f6ec1f95055
xmancheng <104349780+xmancheng@users.noreply.github.com>
2024-07-09 00:30:36 +0800

    Add files via upload

---
+++ Modules/petPalHelp.lua
@@ -0,0 +1,58 @@
+---模块类
+local Module = ModuleBase:createModule('petPalHelp')
+
+local palList = {
+{ petID=401275, type=1, heal=20, pickitem=-1, pickitemNum=0},  --治疗
+{ petID=401276, type=2, heal=0, pickitem=70162, pickitemNum=3},  --采集
+{ petID=401277, type=3, heal=10, pickitem=70163, pickitemNum=3},  --治疗+采集
+
+}
+
+--- 加载模块钩子
+function Module:onLoad()
+  self:logInfo('load')
+  self:regCallback('GatherItemEvent', function(charIndex, skillId, skillLv, itemNo)
+    for Slot=0,4 do
+        local petIndex = Char.GetPet(charIndex, Slot);
+        if (petIndex>0 and Char.GetData(petIndex,CONST.PET_DepartureBattleStatus)==CONST.PET_STATE_战斗) then
+            local PETID = Char.GetData(petIndex,CONST.宠物_PETID);
+            table.forEach(palList, function(e)
+              if (itemNo>0 and PETID == e.petID)  then
+                  if (e.type==2 or e.type==3) then
+                      local burst = NLG.Rand(1, 5);
+                      if (itemNo==e.pickitem and e.pickitemNum>0) then
+                          if (burst==1) then
+                              Char.GiveItem(charIndex, itemNo, e.pickitemNum);
+                              NLG.SortItem(charIndex);
+                              NLG.SystemMessage(charIndex, "[系統]你的寵物幫忙採集多增加"..e.pickitemNum.."個");
+                          end
+                      end
+                  end
+                  if (e.type==1 or e.type==3) then
+                      local injury = Char.GetData(charIndex, CONST.CHAR_受伤);
+                      if ( injury < 1) then
+                          return;
+                      else
+                          if (injury>0 and e.heal>0) then
+                              Char.SetData(charIndex, CONST.CHAR_受伤, injury+e.heal);
+                              NLG.UpChar(charIndex);
+                              NLG.SystemMessage(charIndex, "[系統]你的寵物給予你治療傷勢"..e.heal.."點");
+                          end
+                      end
+                  end
+              end
+            end)
+        end
+    end
+    return;
+  end)
+
+end
+
+
+--- 卸载模块钩子
+function Module:onUnload()
+  self:logInfo('unload')
+end
+
+return Module;
