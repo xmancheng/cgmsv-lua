245a40a80c95d0e66642369c3841431e196ea0c0
xmancheng <104349780+xmancheng@users.noreply.github.com>
2024-07-11 11:14:21 +0800

    Update itemThrow.lua
    更新

--- Modules/itemThrow.lua
+++ Modules/itemThrow.lua
@@ -6,18 +6,20 @@ for hhh = 0,5 do
 	Throw_control[hhh] = false;     --初始化投掷开关
 end]]
 local MaxLv = 250
-local GetitEnable_list = {};
-GetitEnable_list[400014] = {1,400014};  --enemy.txt编号，设置1可捕捉、设置0或无设置不可捕捉
-GetitEnable_list[400015] = {1,400015};
-GetitEnable_list[400125] = {1,401275};
-GetitEnable_list[400126] = {1,401276};
+local BallKind_check= {75011,75018,75019};
+local BallKind_list = {};
+BallKind_list[75011] = 1;    --精靈球
+BallKind_list[75018] = 2;    --超級球
+BallKind_list[75019] = 3;    --高級球
 
-GetitEnable_list[900030] = {1,900032};  --第十二席
-GetitEnable_list[900033] = {1,900035};  --第十一席
-GetitEnable_list[900036] = {1,900038};  --第十席
-GetitEnable_list[900039] = {1,900041};  --第九席
-GetitEnable_list[900042] = {1,900044};  --第八席
-GetitEnable_list[900045] = {1,900047};  --第七席
+local GetitEnable_list = {};
+GetitEnable_list[0] = { open=0, pool={0}};
+GetitEnable_list[400014] = { open=1, pool={400014}};  --enemy.txt编号，设置1可捕捉、设置0或无设置不可捕捉
+GetitEnable_list[400015] = { open=1, pool={400015}};
+GetitEnable_list[400125] = { open=1, pool={401275}};
+GetitEnable_list[400126] = { open=1, pool={401276}};
+
+GetitEnable_list[1003] = { open=1, pool={500017,500018,500019}};  --第十二席
 
 -----------------------------------------------------------------
 function ItemThrow:setItemData(itemIndex, value)
@@ -52,7 +54,7 @@ function ItemThrow:onItemUseEvent(charIndex, targetCharIndex, itemSlot)
       if (battleIndex==-1 and Battle.IsWaitingCommand(charIndex)<=0) then
                NLG.SystemMessage(charIndex,"[道具提示]戰鬥中才能使用的道具");
       else
-            if (ItemID==75011) then
+            if (CheckInTable(BallKind_check,ItemID)==true) then
                for i = 10, 19 do
                      local enemy = Battle.GetPlayer(battleIndex, i);
                      if enemy == targetCharIndex then
@@ -62,7 +64,8 @@ function ItemThrow:onItemUseEvent(charIndex, targetCharIndex, itemSlot)
                end
                --Throw_control[charIndex] = true;
                Char.SetTempData(charIndex, 'PokeBall', 1);
-               Char.DelItem(charIndex,75011,1);
+               Char.SetTempData(charIndex, 'PokeBallKind', BallKind_list[ItemID]);
+               Char.DelItem(charIndex,ItemID,1);
                NLG.Say(charIndex,charIndex,"【準備投擲】下回合才丟出！！",4,3);
             end
       end
@@ -76,8 +79,9 @@ function ItemThrow:battleOverEventCallback(battleIndex)
         local charIndex = Battle.GetPlayer(battleIndex, i);
         if charIndex >= 0 then
                local Poke = Char.GetTempData(charIndex, 'PokeBall') or 0;
-               if Poke==1 then
+               if Poke>=1 then
                  Char.SetTempData(charIndex, 'PokeBall', 0);
+                 Char.SetTempData(charIndex, 'PokeBallKind', 0);
                end
                --Throw_control[charIndex] = false;
         end
@@ -115,13 +119,16 @@ function ItemThrow:OnDamageCalculateCallBack(charIndex, defCharIndex, oriDamage,
          local getit= NLG.Rand(1, math.ceil(HpE05*4) );
          local LvE = math.ceil(Char.GetData(defCharIndex,CONST.CHAR_等级)*0.8);
          local LvMR = NLG.Rand(1,MaxLv);
+         local ballKind = Char.GetTempData(charIndex, 'PokeBallKind') or 0;
+         local CatchRateUp = PokeCatchRate(ballKind);
          if com3 == 200209 and Char.GetData(defCharIndex, CONST.CHAR_类型) == CONST.对象类型_怪  then
-                if getit == 1 and LvMR >= LvE  then
-                        local enemyId = Char.GetData(defCharIndex, CONST.CHAR_ENEMY_ID);
+                if (getit == 1 and (LvMR+CatchRateUp) >= LvE)  then
+                        local enemyId = Char.GetData(defCharIndex, CONST.对象_ENEMY_ID);
                         --local EnemyBaseId = Data.GetEnemyBaseIdByEnemyId(enemyId);
-                        if GetitEnable_list[enemyId][1] == 1  then
+                        if (GetitEnable_list[enemyId].open == 1)  then
                                 --Char.AddPet(charIndex,GetitEnable_list[enemyId][2]);
-                                Char.GivePet(charIndex,GetitEnable_list[enemyId][2],0);
+                                local randCatch= NLG.Rand(1, #GetitEnable_list[enemyId].pool );
+                                Char.GivePet(charIndex,GetitEnable_list[enemyId].pool[randCatch],0);
                                 damage = 7777777;
                                 return damage;
                         else
@@ -141,50 +148,26 @@ function ItemThrow:OnDamageCalculateCallBack(charIndex, defCharIndex, oriDamage,
   return damage;
 end
 
---[[
-function ItemThrow:onBattleActionEvent(charIndex, com1, com2, com3, actionNum)
-  if com1 == 9 then
-    local itemSlot = com3
-    local itemIndex = Char.GetItemIndex(charIndex,itemSlot);
-    local battleIndex = Char.GetBattleIndex(charIndex);
-    local ItemID = Item.GetData(itemIndex, CONST.道具_ID);
-    --local boom = Char.HaveSkill(charIndex,2002);
-    --local skillLV = Char.GetSkillLevel(charIndex,boom);
-    --local itemlv = Item.GetData(itemIndex, CONST.道具_等级)
-    if (Item.GetData(itemIndex, CONST.道具_类型)==51) then
-           --第一階段(準備)
-           --local enemySlot = com2
-           --local Throw_pos = enemySlot+20;
-           --Throw_control[charIndex] = true;
-           --Char.DelItem(charIndex,ItemID,1);
-           --NLG.Say(charIndex,charIndex,"【準備投擲】下回合建議防禦！！",4,3);
-           --第二階段(投擲)
-           for i = 0, 19 do
-                  local charIndex = Battle.GetPlayer(battleIndex, i);
-                  if charIndex >= 0 then
-                         local sidetable = {{10,40,41,30,20},{0,41,40,30,20}}
-                         local charside = 1
-                         local ybside = Char.GetData(charIndex,%对象_战斗Side%)
-                         if ybside == 1 then
-                                charside = 2
-                         end
-                         local ybjn = Battle.IsWaitingCommand(charIndex);
-                         local Poke = Char.GetTempData(charIndex, 'PokeBall') or 0;
-                         if ybjn and Poke==1 and actionNum == 1  then
-                                --lv = tostring(skillLV-1)
-                                --lv = ("20020"..lv)
-                                --Battle.ActionSelect(CharIndex, CONST.BATTLE_COM.BATTLE_COM_THROWITEM, sidetable[charside][3], tonumber(lv));
-                                Battle.ActionSelect(charIndex, CONST.BATTLE_COM.BATTLE_COM_THROWITEM, Throw_pos, 200209);
-                                --Battle.ActionSelect(charIndex, CONST.BATTLE_COM.BATTLE_COM_P_SPIRACLESHOT, sidetable[charside][1], 403);
-                                --Throw_control[charIndex] = false;
-                         end
-                  end
-           end
-    end
-  end
-  return;
+function PokeCatchRate(ballKind)
+         local catchRateUp = 0;
+         if ballKind==1 then
+             catchRateUp = 5;
+         elseif ballKind==2 then
+             catchRateUp = 10;
+         elseif ballKind==3 then
+             catchRateUp = 15;
+         end
+    return catchRateUp;
 end
-]]
+
+function CheckInTable(_idTab, _idVar) ---循环函数
+	for k,v in pairs(_idTab) do
+		if v==_idVar then
+			return true
+		end
+	end
+	return false
+end
 
 function ItemThrow:onUnload()
   self:logInfo('unload')
