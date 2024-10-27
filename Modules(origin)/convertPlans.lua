36e14a2612997d3acf1b2b72d3524f852714aede
xmancheng <104349780+xmancheng@users.noreply.github.com>
2024-10-27 23:58:40 +0800

    Add files via upload
    未完成備份

---
+++ Modules(origin)/convertPlans.lua
@@ -0,0 +1,234 @@
+---模块类
+local Module = ModuleBase:createModule(convertPlans)
+
+--分类自行添加
+local convert_plan_name = {};
+local convert_plan_offering = {};
+local convert_plan_item = {};
+local convert_plan_gold = {};
+local convert_plan_pet = {};
+--
+convert_plan_name[1] = "火影忍者系列001";
+convert_plan_offering[1] = {5,831,101};
+convert_plan_item[1] = {79064};
+convert_plan_gold[1] =15000;
+convert_plan_pet[1] = {5,5,5,5,5,5,5,500057,500057,606093};
+
+convert_plan_name[2] = "航海王系列001";
+convert_plan_offering[2] = {503,801,104};
+convert_plan_item[2] = {79065};
+convert_plan_gold[2] =15000;
+convert_plan_pet[2] = {503,503,503,503,503,503,503,500063,500063,606092};
+-------------------------------------------------
+local function calcWarp()
+  local page = math.modf(#convert_plan_name / 8) + 1
+  local remainder = math.fmod(#convert_plan_name, 8)
+  return page, remainder
+end
+
+--远程按钮UI呼叫
+function Module:convertPlansInfo(npc, player)
+          local winButton = CONST.BUTTON_关闭;
+          local msg = "1\\n　　　　　　　　【寵物異變改造】\\n"
+          for i = 1,#convert_plan_name do
+             msg = msg .. "　　項目: "..i.."　".. convert_plan_name[i] .. "\\n"
+             if (i>=8) then
+                 winButton = CONST.BUTTON_下取消;
+             end
+          end
+          NLG.ShowWindowTalked(player, self.converterNPC, CONST.窗口_选择框, winButton, 1, msg);
+end
+
+--- 加载模块钩子
+function Module:onLoad()
+  self:logInfo('load');
+  self.converterNPC = self:NPC_createNormal('寵物異變改造', 14682, { x = 38, y = 31, mapType = 0, map = 777, direction = 6 });
+  self:NPC_regWindowTalkedEvent(self.converterNPC, function(npc, player, _seqno, _select, _data)
+    local column = tonumber(_data)
+    local page = tonumber(_seqno)
+    local warpPage = page;
+    local winMsg = "1\\n　　　　　　　　【寵物異變改造】\\n"
+    local winButton = CONST.BUTTON_关闭;
+    local totalPage, remainder = calcWarp()
+    --上页16 下页32 关闭/取消2
+    if _select > 0 then
+      if _select == CONST.按钮_确定 then
+          if (page>=1001) then
+              local seqno = page - 1000;
+              local msg = "　　　　　　　　【寵物異變改造】\\n"
+                                  .. "　　　$1需要以下所有材料才能進行異變改造\\n"
+              for i = 1,#convert_plan_offering[seqno] do
+                  EnemyDataIndex_i = Data.EnemyGetDataIndex(convert_plan_offering[seqno][i]);
+                  enemyBaseId_i = Data.EnemyGetData(EnemyDataIndex_i, CONST.Enemy_Base编号);
+                  EnemyBaseDataIndex_i = Data.EnemyBaseGetDataIndex(enemyBaseId_i);
+                  offering_name_i = Data.EnemyBaseGetData(EnemyBaseDataIndex_i, CONST.EnemyBase_名字);
+                  offering_image_i = Data.EnemyBaseGetData(EnemyBaseDataIndex_i, CONST.EnemyBase_形象);
+                  offering_image_ix = 3 + 7*(i-1);
+                  imageText_i = "@g,"..offering_image_i..","..offering_image_ix..",5,4,0@"
+                  msg = msg .. "　".. offering_name_i .. " Lv1　" .. imageText_i
+              end
+              local Gold = convert_plan_gold[seqno];
+              local ItemsetIndex = Data.ItemsetGetIndex(convert_plan_item[seqno][1]);
+              local Item_name= Data.ItemsetGetData( ItemsetIndex, CONST.ITEMSET_TRUENAME);
+              local probRate = prob(seqno,convert_plan_pet[seqno][10]);
+              local msg = msg .. "\\n\\n\\n\\n\\n　$5道具 ".. Item_name .. "1個" .. "　　$5魔幣 " .. Gold .. " G\\n"
+                                              .. "　$4成功機率 ".. probRate .. "%" .. "　　　$9失敗殘念品 第一順位之寵物"
+              NLG.ShowWindowTalked(player, self.converterNPC, CONST.窗口_信息框, CONST.按钮_是否, 2000+seqno, msg);
+              return
+          else
+              return
+          end
+      elseif _select == CONST.按钮_是 then
+          if (page>=2001) then
+              return
+          else
+              return
+          end
+      elseif _select == CONST.按钮_否 then
+        return
+      end
+      if _select == CONST.BUTTON_下一页 then
+        warpPage = warpPage + 1
+        if (warpPage == totalPage) or ((warpPage == (totalPage - 1) and remainder == 0)) then
+          winButton = CONST.BUTTON_上取消
+        else
+          winButton = CONST.BUTTON_上下取消
+        end
+      elseif _select == CONST.BUTTON_上一页 then
+        warpPage = warpPage - 1
+        if warpPage == 1 then
+          winButton = CONST.BUTTON_下取消
+        else
+          winButton = CONST.BUTTON_上下取消
+        end
+      elseif _select == 2 then
+        warpPage = 1
+        return
+      end
+      local count = 8 * (warpPage - 1)
+      if warpPage == totalPage then
+        for i = 1 + count, remainder + count do
+            winMsg = winMsg .. "　　項目: "..i.."　".. convert_plan_name[i] .. "\\n"
+        end
+      else
+        for i = 1 + count, 8 + count do
+            winMsg = winMsg .. "　　項目: "..i.."　".. convert_plan_name[i] .. "\\n"
+        end
+      end
+      NLG.ShowWindowTalked(player, npc, CONST.窗口_选择框, winButton, warpPage, winMsg);
+    else
+      local count = 8 * (warpPage - 1) + column
+      print(count)
+      local msg = "　　　　　　　　【寵物異變改造】\\n"
+      local msg = msg .. convertGoalInfo(count);
+      NLG.ShowWindowTalked(player, self.converterNPC, CONST.窗口_信息框, CONST.按钮_确定关闭, 1000+count, msg);
+    end
+  end)
+  self:NPC_regTalkedEvent(self.converterNPC, function(npc, player)
+    if (NLG.CanTalk(npc, player) == true) then
+      local winButton = CONST.BUTTON_关闭;
+      local msg = "1\\n　　　　　　　　【寵物異變改造】\\n"
+      for i = 1,#convert_plan_name do
+         msg = msg .. "　　項目: "..i.."　".. convert_plan_name[i] .. "\\n"
+         if (i>=8) then
+             winButton = CONST.BUTTON_下取消;
+         end
+      end
+      NLG.ShowWindowTalked(player, npc, CONST.窗口_选择框, winButton, 1, msg);
+    end
+    return
+  end)
+end
+
+function convertGoalInfo(count)
+      local EnemyDataIndex_Goal = Data.EnemyGetDataIndex(convert_plan_pet[count][10]);
+      local enemyBaseId_Goal = Data.EnemyGetData(EnemyDataIndex_Goal, CONST.Enemy_Base编号);
+      local EnemyBaseDataIndex_Goal = Data.EnemyBaseGetDataIndex(enemyBaseId_Goal);
+      local Goal_name = Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_名字);
+      local Goal_DataPos_3 = Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_种族);
+      local Goal_DataPos_3 = Tribe(Goal_DataPos_3);
+      local Goal_DataPos_4 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_体力);
+      local Goal_DataPos_5 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_力量);
+      local Goal_DataPos_6 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_强度);
+      local Goal_DataPos_7 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_速度);
+      local Goal_DataPos_8 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_魔法);
+      local Goal_DataPos_12 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_命中);
+      local Goal_DataPos_13 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_必杀);
+      local Goal_DataPos_14 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_地属性);
+      local Goal_DataPos_15 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_水属性);
+      local Goal_DataPos_16 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_火属性);
+      local Goal_DataPos_17 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_风属性);
+      local Goal_DataPos_18 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_抗毒);
+      local Goal_DataPos_19 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_抗醉);
+      local Goal_DataPos_20 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_抗睡);
+      local Goal_DataPos_21 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_抗混乱);
+      local Goal_DataPos_22 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_抗石化);
+      local Goal_DataPos_23 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_抗遗忘);
+      local Goal_DataPos_26 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_闪躲);
+      local Goal_DataPos_27 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_反击);
+      local Goal_DataPos_28 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_技能栏);
+      local Goal_DataPos_29 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_形象);
+      local imageText = "@g,"..Goal_DataPos_29..",3,10,6,0@"
+      local Goal_DataPos_35 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_出生技能1);
+      local Goal_DataPos_36 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_出生技能2);
+      local Goal_DataPos_37 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_出生技能3);
+      local Goal_DataPos_38 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_出生技能4);
+      local Goal_DataPos_39 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_出生技能5);
+      local Goal_DataPos_40 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_出生技能6);
+      local Goal_DataPos_41 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_出生技能7);
+      local Goal_DataPos_42 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_出生技能8);
+      local Goal_DataPos_43 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_出生技能9);
+      local Goal_DataPos_44 =Data.EnemyBaseGetData(EnemyBaseDataIndex_Goal, CONST.EnemyBase_出生技能10);
+      local msg = imageText .. "　　".. Goal_name .. "\\n"
+                         .. "　　　　　　　" .. "$1體力 ".. Goal_DataPos_4+2 .."　" .. "$8必殺 ".. Goal_DataPos_13 .."　" .. "$8反擊 ".. Goal_DataPos_27 .."\\n"
+                         .. "　　　　　　　" .. "$1力量 ".. Goal_DataPos_5+2 .."　" .. "$8命中 ".. Goal_DataPos_12 .."　" .. "$8閃躲 ".. Goal_DataPos_26 .."\\n"
+                         .. "　　　　　　　" .. "$1強度 ".. Goal_DataPos_6+2 .."　" .. "$2抗毒 ".. Goal_DataPos_18 .."　" .. "$2抗醉 ".. Goal_DataPos_19 .."\\n"
+                         .. "　　　　　　　" .. "$1速度 ".. Goal_DataPos_7+2 .."　" .. "$2抗睡 ".. Goal_DataPos_20 .."　" .. "$2抗混 ".. Goal_DataPos_21 .."\\n"
+                         .. "　　　　　　　" .. "$1魔法 ".. Goal_DataPos_8+2 .."　" .. "$2抗石 ".. Goal_DataPos_22 .."　" .. "$2抗忘 ".. Goal_DataPos_23 .."\\n"
+                         .. "　　　　　　　" .. "$5地 ".. Goal_DataPos_14 .."　" .."$5水 ".. Goal_DataPos_15 .."　" .."$5火 ".. Goal_DataPos_16 .."　" .."$5風 ".. Goal_DataPos_17 .."\\n"
+                         .. "　　　　　　　" .. "$9種族 ".. Goal_DataPos_3 .."　" .. "$9技能格 ".. Goal_DataPos_28 .."\\n"
+      return msg;
+end
+function prob(count,id)
+  for i=1,10 do
+      if (convert_plan_pet[count][i]==id) then
+          local prob = ( (11-i)/10 )*100;
+          return prob;
+      end
+  end
+  return -1;
+end
+function Tribe(Tribe)
+  if Tribe==0 then
+    return "人型系"
+  elseif Tribe == 1 then
+    return "龍族系"
+  elseif Tribe == 2 then
+    return "不死系"
+  elseif Tribe == 3 then
+    return "飛行系"
+  elseif Tribe == 4 then
+    return "昆蟲系"
+  elseif Tribe == 5 then
+    return "植物系"
+  elseif Tribe == 6 then
+    return "野獸系"
+  elseif Tribe == 7 then
+    return "特殊系"
+  elseif Tribe == 8 then
+    return "金屬系"
+  elseif Tribe == 9 then
+    return "邪魔系"
+  elseif Tribe == 10 then
+    return "神族系"
+  elseif Tribe == 11 then
+    return "精靈系"
+  end
+end
+
+--- 卸载模块钩子
+function Module:onUnload()
+  self:logInfo('unload')
+end
+
+return Module;
