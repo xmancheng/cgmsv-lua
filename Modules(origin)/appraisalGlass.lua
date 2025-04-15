6f58b58a1b6ec3ec7eb9c956a0691b78cb216d6f
xmancheng <104349780+xmancheng@users.noreply.github.com>
2025-04-15 16:27:10 +0800

    Add files via upload

---
+++ Modules(origin)/appraisalGlass.lua
@@ -0,0 +1,227 @@
+---模块类
+local Module = ModuleBase:createModule('appraisalGlass')
+
+local specialEffects = {
+  {4,20,35,1640736},	--記載著拉傑法爾的偷襲心得
+  {4,55,35,1020716},	--裝備上就會擁有王者的保護，可以輕鬆的襲擊魔族
+  --{12,50,0,3012354},	--能力值變為50級的狀態
+  --{13,0,0,3012354},	--偷取敵人物品時，同時能給予敵人傷害力
+  --{13,1,0,3004870},	--使用護衛技能時能發動反擊
+  --{13,3,0,3004873},	--使用明鏡止水技能時隨機回避敵人的攻擊
+  {2,62,50,1645959},	--可使技能‘強力補血’耗魔減半
+  {2,23,50,1645960},	--可使技能‘強力隕石’耗魔減半
+  {2,24,50,1645961},	--可使技能‘強力冰凍’耗魔減半
+  {2,25,50,1645962},	--可使技能‘強力火焰’耗魔減半
+  {2,26,50,1645963},	--可使技能‘強力風刃’耗魔減半
+  {2,61,50,1645972},	--可使技能‘補血魔法’耗魔減半
+  {2,40,50,1645973},	--可使技能‘強力石化’耗魔減半
+  {2,39,50,1645974},	--可使技能‘強力昏睡’耗魔減半
+  {2,42,50,1645975},	--可使技能‘強力混亂’耗魔減半
+  {2,43,50,1645976},	--可使技能‘強力遺忘’耗魔減半
+  {2,44,50,1645977},	--可使技能‘強力中毒’耗魔減半
+  {2,41,50,1645978},	--可使技能‘強力酒醉’耗魔減半
+  {6,8,0,1645919},	--可以變化成金屬系
+  {6,7,0,1645918},	--可以變化成特殊系
+  {6,6,0,1645917},	--可以變化成野獸系
+  {6,5,0,1645916},	--可以變化成植物系
+  {6,4,0,1645915},	--可以變化成昆蟲系
+  {6,3,0,1645914},	--可以變化成飛行系
+  {6,2,0,1645913},	--可以變化成不死系
+  {6,1,0,1645912},	--可以變化成龍系
+}
+------------------------------------------------
+--- 加载模块钩子
+function Module:onLoad()
+  self:logInfo('load')
+  --鑑定镜道具
+  self:regCallback('ItemString', Func.bind(self.actifyGlass, self),"LUA_useActiGlas");
+  AppraisalGlassNPC = self:NPC_createNormal('鑑定鏡', 103013, { x = 38, y = 33, mapType = 0, map = 777, direction = 6 });
+  Char.SetData(AppraisalGlassNPC,CONST.对象_ENEMY_PetFlg+2,0)--可穿透体
+  self:NPC_regTalkedEvent(AppraisalGlassNPC, function(npc, player)
+    if (NLG.CanTalk(npc, player) == true) then
+        local GoalIndex,GoalSlot = Char.GetUnidentifiedSlot(player);
+        if (GoalIndex>0) then
+          local GoalName = Item.GetData(GoalIndex,CONST.道具_名字);
+                local msg = "\\n\\n@c【系統通知】"
+                  .."\\n\\n"
+                  .."\\n自動找尋未鑑定物品...\\n"
+                  .."\\n確定要鑑定 $5"..GoalName.." $0？\\n";
+          NLG.ShowWindowTalked(player, AppraisalGlassNPC, CONST.窗口_信息框, CONST.按钮_是否, 1, msg);
+        else
+                local msg = "\\n\\n@c【系統通知】"
+                  .."\\n\\n"
+                  .."\\n自動找尋未鑑定物品...\\n"
+                  .."\\n物品欄中沒有需要鑑定的道具。\\n";
+          NLG.ShowWindowTalked(player, AppraisalGlassNPC, CONST.窗口_信息框, CONST.按钮_关闭, 1, msg);
+        end
+    end
+    return
+  end)
+  self:NPC_regWindowTalkedEvent(AppraisalGlassNPC, function(npc, player, _seqno, _select, _data)
+    local seqno = tonumber(_seqno)
+    local select = tonumber(_select)
+    local data = tonumber(_data)
+    local GlassIndex = GlassIndex;
+    local GlassSlot = GlassSlot;
+    local GoalIndex,GoalSlot = Char.GetUnidentifiedSlot(player);
+    if select > 0 then
+      if seqno == 1 and select == CONST.按钮_关闭 then
+          return;
+      elseif seqno == 1 and select == CONST.按钮_否 then
+          return;
+      elseif seqno == 1 and select == CONST.按钮_是 then
+        if (GoalIndex>0) then
+          local Special = Item.GetData(GoalIndex,CONST.道具_特殊类型);
+          local Para1 = Item.GetData(GoalIndex,CONST.道具_子参一);
+          local Para2 = Item.GetData(GoalIndex,CONST.道具_子参二);
+          if (Special==0) then
+              local rand = NLG.Rand(1,#specialEffects);
+              Item.SetData(GoalIndex,CONST.道具_特殊类型, specialEffects[rand][1]);
+              Item.SetData(GoalIndex,CONST.道具_子参一, specialEffects[rand][2]);
+              Item.SetData(GoalIndex,CONST.道具_子参二, specialEffects[rand][3]);
+              Item.SetData(GoalIndex,CONST.道具_Explanation1,specialEffects[rand][4]);
+              Item.SetData(GoalIndex,CONST.道具_幸运, 66);
+              Item.SetData(GoalIndex,CONST.道具_已鉴定,1);
+              Item.UpItem(player, GoalSlot);
+              NLG.UpChar(player);
+              Char.DelItem(player, 75031, 1);
+              NLG.SystemMessage(player,"[系统]"..Item.GetData(GoalIndex,CONST.道具_鉴前名).."鑑定成功 "..Item.GetData(GoalIndex,CONST.道具_名字).."。");
+          else
+              Item.SetData(GoalIndex,CONST.道具_已鉴定,1);
+              Item.UpItem(player, GoalSlot);
+              NLG.UpChar(player);
+              Char.DelItem(player, 75031, 1);
+              NLG.SystemMessage(player,"[系统]"..Item.GetData(GoalIndex,CONST.道具_鉴前名).."鑑定成功 "..Item.GetData(GoalIndex,CONST.道具_名字).."。");
+          end
+        else
+            return;
+        end
+      else
+          return;
+      end
+    end
+  end)
+
+
+  --附加效果
+  AdditionalEffectsNPC = self:NPC_createNormal('鑑定效果轉移', 14602, { x = 246, y = 76, mapType = 0, map = 1000, direction = 6 });
+  Char.SetData(AdditionalEffectsNPC,CONST.对象_ENEMY_PetFlg+2,0)--可穿透体
+  self:NPC_regTalkedEvent(AdditionalEffectsNPC, function(npc, player)
+    if (NLG.CanTalk(npc, player) == true) then
+        local ItemIndex = Char.GetItemIndex(player, CONST.EQUIP_首饰2);
+        if (ItemIndex < 0) then
+            NLG.SystemMessage(player,"[系統]飾品2位置沒有任何道具。");
+            return;
+        elseif (ItemIndex >= 0) then
+            local CarrierIndex,CarrierSlot = Char.GetIdentifiedSlot(player);
+            if (CarrierIndex>0 and Item.GetData(ItemIndex, CONST.道具_类型)==62) then
+                local CarrierName = Data.GetMessage(Item.GetData(CarrierIndex,CONST.道具_Explanation1));
+                local msg = "\\n\\n@c【系統通知】"
+                    .."\\n\\n"
+                    .."\\n確定要轉移 $5"..CarrierName.."\\n"
+                    .."\\n覆蓋掉影鏡原有的特殊效果嗎？\\n";
+                NLG.ShowWindowTalked(player, AdditionalEffectsNPC, CONST.窗口_信息框, CONST.按钮_是否, 1, msg);
+            elseif (CarrierIndex>0 and Item.GetData(ItemIndex, CONST.道具_类型)~=62) then
+                NLG.SystemMessage(player,"[系統]飾品2須裝備上影鏡。");
+                return;
+            elseif (CarrierIndex<0) then
+                local msg = "\\n\\n@c【系統通知】"
+                    .."\\n\\n"
+                    .."\\n自動找尋已經鑑定的物品...\\n"
+                    .."\\n$4※物品欄中沒有特殊效果的道具。\\n";
+                NLG.ShowWindowTalked(player, AdditionalEffectsNPC, CONST.窗口_信息框, CONST.按钮_关闭, 1, msg);
+            end
+        end
+    end
+    return
+  end)
+  self:NPC_regWindowTalkedEvent(AdditionalEffectsNPC, function(npc, player, _seqno, _select, _data)
+    local seqno = tonumber(_seqno)
+    local select = tonumber(_select)
+    local data = tonumber(_data)
+    local ItemIndex = Char.GetItemIndex(player, CONST.EQUIP_首饰2);
+    local CarrierIndex,CarrierSlot = Char.GetIdentifiedSlot(player);
+    if select > 0 then
+      if seqno == 1 and select == CONST.按钮_关闭 then
+          return;
+      elseif seqno == 1 and select == CONST.按钮_否 then
+          return;
+      elseif seqno == 1 and select == CONST.按钮_是 then
+        if (ItemIndex>0 and CarrierIndex>0) then
+          local Special = Item.GetData(CarrierIndex,CONST.道具_特殊类型);
+          local Para1 = Item.GetData(CarrierIndex,CONST.道具_子参一);
+          local Para2 = Item.GetData(CarrierIndex,CONST.道具_子参二);
+          local info = Item.GetData(CarrierIndex,CONST.道具_Explanation1);
+
+          Item.SetData(ItemIndex,CONST.道具_特殊类型, Special);
+          Item.SetData(ItemIndex,CONST.道具_子参一, Para1);
+          Item.SetData(ItemIndex,CONST.道具_子参二, Para2);
+          Item.SetData(ItemIndex,CONST.道具_Explanation1, info);
+          Item.UpItem(player, CONST.EQUIP_首饰2);
+          Char.DelItemBySlot(player, CarrierSlot);
+          NLG.UpChar(player);
+          NLG.SystemMessage(player,"[系统]特殊效果轉移成功。");
+        else
+            return;
+        end
+      else
+          return;
+      end
+    end
+  end)
+
+end
+------------------------------------------------
+----
+function Module:actifyGlass(charIndex,targetIndex,itemSlot)
+    GlassItemID = Item.GetData(Char.GetItemIndex(charIndex,itemSlot),0);
+    GlassSlot =itemSlot;
+    GlassIndex = Char.GetItemIndex(charIndex,itemSlot);
+    local GoalIndex,GoalSlot = Char.GetUnidentifiedSlot(charIndex);
+    if (GoalIndex>0) then
+      local GoalName = Item.GetData(GoalIndex,CONST.道具_名字);
+            local msg = "\\n\\n@c【系統通知】"
+              .."\\n\\n"
+              .."\\n自動找尋未鑑定物品...\\n"
+              .."\\n確定要鑑定 $5"..GoalName.." $0？\\n";
+      NLG.ShowWindowTalked(charIndex, AppraisalGlassNPC, CONST.窗口_信息框, CONST.按钮_是否, 1, msg);
+    else
+            local msg = "\\n\\n@c【系統通知】"
+              .."\\n\\n"
+              .."\\n自動找尋未鑑定物品...\\n"
+              .."\\n物品欄中沒有需要鑑定的道具。\\n";
+      NLG.ShowWindowTalked(charIndex, AppraisalGlassNPC, CONST.窗口_信息框, CONST.按钮_关闭, 1, msg);
+    end
+    return 1;
+end
+
+Char.GetUnidentifiedSlot = function(charIndex)
+  for Slot=7,27 do
+      local ItemIndex = Char.GetItemIndex(charIndex, Slot);
+      --print(ItemIndex);
+      if (ItemIndex > 0 and Item.GetData(ItemIndex,CONST.道具_已鉴定)==0) then
+        local ItemId = Item.GetData(ItemIndex,CONST.道具_ID);
+        return ItemIndex,Slot;
+      end
+  end
+  return -1,-1;
+end
+
+Char.GetIdentifiedSlot = function(charIndex)
+  for Slot=7,27 do
+      local ItemIndex = Char.GetItemIndex(charIndex, Slot);
+      --print(ItemIndex);
+      if (ItemIndex > 0 and Item.GetData(ItemIndex,CONST.道具_幸运)==66) then
+        local ItemId = Item.GetData(ItemIndex,CONST.道具_ID);
+        return ItemIndex,Slot;
+      end
+  end
+  return -1,-1;
+end
+
+--- 卸载模块钩子
+function Module:onUnload()
+  self:logInfo('unload')
+end
+
+return Module;
---
+++ Modules(origin)/digitalGate.lua
@@ -0,0 +1,144 @@
+---模块类
+local Module = ModuleBase:createModule('digitalGate')
+
+
+--背景设置
+local Pts= 70075;		--70206真女神苹果.70075任务币
+------------------------------------------------
+--- 加载模块钩子
+function Module:onLoad()
+  self:logInfo('load')
+  --数码门道具
+  self:regCallback('ItemString', Func.bind(self.digitalTravel, self),"LUA_useDigiTrip");
+  DigitalGateNPC = self:NPC_createNormal('數碼旅行', 103013, { x = 42, y = 31, mapType = 0, map = 777, direction = 6 });
+  Char.SetData(DigitalGateNPC,CONST.对象_ENEMY_PetFlg+2,0)--可穿透体
+  self:NPC_regTalkedEvent(DigitalGateNPC, function(npc, player)
+    if (NLG.CanTalk(npc, player) == true) then
+                local msg = "\\n\\n@c【系統通知】"
+                  .."\\n\\n"
+                  .."\\n未知的傳送門出現\\n"
+                  .."\\n是否進入數碼世界？\\n";
+        NLG.ShowWindowTalked(player, DigitalGateNPC, CONST.窗口_信息框, CONST.按钮_是否, 1, msg);
+    end
+    return
+  end)
+  self:NPC_regWindowTalkedEvent(DigitalGateNPC, function(npc, player, _seqno, _select, _data)
+    local seqno = tonumber(_seqno)
+    local select = tonumber(_select)
+    local data = tonumber(_data)
+    local ComIndex =ComIndex;
+    local ComSlot = ComSlot;
+    local Target_FloorId = Char.GetData(player,CONST.对象_地图)--地图编号
+    local Target_X = Char.GetData(player,CONST.对象_X)--地图x
+    local Target_Y = Char.GetData(player,CONST.对象_Y)--地图y
+    --local playerLv = Char.GetData(player,CONST.对象_等级);
+    local playerLv = calcTeamLevel(player);
+    if select > 0 then
+      if seqno == 1 and select == CONST.按钮_否 then
+          Char.SetData(npc,CONST.对象_X, 42);
+          Char.SetData(npc,CONST.对象_Y, 31);
+          Char.SetData(npc,CONST.对象_地图, 777);
+          NLG.UpChar(npc); 
+          return;
+      elseif seqno == 1 and select == CONST.按钮_是 then
+          if (Char.ItemNum(player, 75030)<1) then
+              NLG.SystemMessage(player,"[系統]你沒有進入資格。");
+              return;
+          end
+          if (playerLv>=1 and playerLv<=49) then
+              Char.Warp(player,0, 25009, 78, 19);
+              Char.DelItem(player, 75030, 1);
+              NLG.SystemMessage(player, "[系统]進入數碼世界。")
+          elseif (playerLv>=50 and playerLv<=100) then
+              Char.Warp(player,0, 25009, 48, 19);
+              Char.DelItem(player, 75030, 1);
+              NLG.SystemMessage(player, "[系统]進入數碼世界。")
+          elseif (playerLv>=101) then
+              Char.Warp(player,0, 25009, 18, 19);
+              Char.DelItem(player, 75030, 1);
+              NLG.SystemMessage(player, "[系统]進入數碼世界。")
+          end
+          Char.SetData(npc,CONST.对象_X, 42);
+          Char.SetData(npc,CONST.对象_Y, 31);
+          Char.SetData(npc,CONST.对象_地图, 777);
+          NLG.UpChar(npc); 
+      else
+          return;
+      end
+    end
+  end)
+
+end
+------------------------------------------------
+----
+function Module:digitalTravel(charIndex,targetIndex,itemSlot)
+    ComItemID = Item.GetData(Char.GetItemIndex(charIndex,itemSlot),0);
+    ComSlot =itemSlot;
+    ComIndex = Char.GetItemIndex(charIndex,itemSlot);
+    local Target_FloorId = Char.GetData(charIndex,CONST.对象_地图);
+	local Target_X = Char.GetData(charIndex,CONST.对象_X);
+	local Target_Y = Char.GetData(charIndex,CONST.对象_Y);
+	local warpX = NLG.Rand(Target_X-5, Target_X+5);
+	local warpY = NLG.Rand(Target_Y-5, Target_Y+5);
+	if (Target_FloorId~=25009) then
+	  Char.SetData(DigitalGateNPC,CONST.对象_X, warpX);
+	  Char.SetData(DigitalGateNPC,CONST.对象_Y, warpY);
+	  Char.SetData(DigitalGateNPC,CONST.对象_地图, Target_FloorId);
+	  NLG.UpChar(DigitalGateNPC);
+      Char.Warp(charIndex,0, Target_FloorId, Target_X, Target_Y);
+	  NLG.SystemMessage(charIndex,"[系統]未知的傳送門出現在附近("..warpX..","..warpY..")");
+    else
+      NLG.SystemMessage(charIndex,"[系統]數碼世界中無法再開傳送門");
+      return 1;
+	end
+--[[
+                local msg = "\\n\\n@c【系統通知】"
+                  .."\\n\\n"
+                  .."\\n未知的傳送門出現\\n"
+                  .."\\n是否進入數碼世界？\\n";
+    NLG.ShowWindowTalked(charIndex, DigitalGateNPC, CONST.窗口_信息框, CONST.按钮_是否, 1, msg);
+]]
+    return 1;
+end
+
+function calcTeamLevel(player)
+	local m = 0;
+	local k = 0;
+	for slot = 0,4 do
+		local p = Char.GetPartyMember(player,slot);
+		if (p>=0) then
+			m = m + Char.GetData(p,CONST.对象_等级);
+			k = k+1;
+		end
+	end
+	--计算平均等级及等第
+	local lv = math.floor(m/k);
+	return lv;
+end
+
+function heads(CoinNo)
+	local h,r,s = 0,0,0;
+	local result_tbl = {};
+	for i=1,CoinNo do
+		local result = NLG.Rand(0,1);
+		table.insert(result_tbl,result);
+	end
+	for k,v in pairs(result_tbl) do
+		if (v==1) then
+			h = h + 1;
+		elseif (v==0) then
+			r = r + 1;
+		end
+	end
+	if (h==#result_tbl or r==#result_tbl) then
+		s = 1;
+	end
+	return h,r,s
+end
+
+--- 卸载模块钩子
+function Module:onUnload()
+  self:logInfo('unload')
+end
+
+return Module;
