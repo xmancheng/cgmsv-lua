f3c69fd5dab9b2fa98c609ac000ba0da5bbab112
xmancheng <104349780+xmancheng@users.noreply.github.com>
2025-10-19 18:15:13 +0800

    Add files via upload
    寵裝皮膚

---
+++ Modules(poke)/petSkin.lua
@@ -0,0 +1,336 @@
+local Module = ModuleBase:createModule('petSkin')
+
+local skinImage_List = {};
+skinImage_List[69101] = {};
+skinImage_List[69101][101004] = 123084;					--道具编号.原始形象编号.新形象编号
+skinImage_List[69101][101000] = 123085;
+skinImage_List[69101][101002] = 123086;
+skinImage_List[69101][101001] = 123087;
+skinImage_List[69101][101005] = 123088;
+
+skinImage_List[69102] = {};
+skinImage_List[69102][101010] = 123155;
+skinImage_List[69102][101011] = 123152;
+skinImage_List[69102][101012] = 123151;
+skinImage_List[69102][101013] = 123153;
+skinImage_List[69102][101014] = 123154;
+
+skinImage_List[69103] = {};
+skinImage_List[69103][101020] = 123045;
+skinImage_List[69103][101021] = 123046;
+skinImage_List[69103][101022] = 123047;
+skinImage_List[69103][101023] = 123044;
+skinImage_List[69103][101024] = 123048;
+skinImage_List[69103][101025] = 123049;
+
+skinImage_List[69104] = {};
+skinImage_List[69104][101030] = 123050;
+skinImage_List[69104][101031] = 123053;
+skinImage_List[69104][101032] = 123051;
+skinImage_List[69104][101033] = 123052;
+
+skinImage_List[69105] = {};
+skinImage_List[69105][101100] = 123065;
+skinImage_List[69105][101101] = 123068;
+skinImage_List[69105][101102] = 123069;
+skinImage_List[69105][101103] = 123066;
+skinImage_List[69105][101104] = 123067;
+
+skinImage_List[69106] = {};
+skinImage_List[69106][101120] = 123176;
+skinImage_List[69106][101121] = 123177;
+skinImage_List[69106][101122] = 123174;
+skinImage_List[69106][101123] = 123175;
+
+skinImage_List[69107] = {};
+skinImage_List[69107][101200] = 123113;
+skinImage_List[69107][101201] = 123109;
+skinImage_List[69107][101202] = 123111;
+skinImage_List[69107][101203] = 123112;
+skinImage_List[69107][101204] = 123110;
+skinImage_List[69107][101205] = 123114;
+
+skinImage_List[69108] = {};
+skinImage_List[69108][101210] = 123058;
+skinImage_List[69108][101211] = 123059;
+skinImage_List[69108][101212] = 123056;
+skinImage_List[69108][101213] = 123057;
+
+skinImage_List[69109] = {};
+skinImage_List[69109][101240] = 123137;
+skinImage_List[69109][101241] = 123139;
+skinImage_List[69109][101242] = 123138;
+skinImage_List[69109][101243] = 123140;
+skinImage_List[69109][101244] = 123136;
+skinImage_List[69109][101245] = 123141;
+
+skinImage_List[69110] = {};
+skinImage_List[69110][101250] = 123023;
+skinImage_List[69110][101251] = 123025;
+skinImage_List[69110][101252] = 123024;
+skinImage_List[69110][101253] = 123026;
+skinImage_List[69110][101254] = 123027;
+
+skinImage_List[69111] = {};
+skinImage_List[69111][101300] = 123147;
+skinImage_List[69111][101301] = 123149;
+skinImage_List[69111][101302] = 123150;
+skinImage_List[69111][101303] = 123148;
+
+skinImage_List[69112] = {};
+skinImage_List[69112][101310] = 123092;
+skinImage_List[69112][101311] = 123093;
+skinImage_List[69112][101312] = 123090;
+skinImage_List[69112][101313] = 123091;
+skinImage_List[69112][101314] = 123089;
+
+skinImage_List[69113] = {};
+skinImage_List[69113][101320] = 123121;
+skinImage_List[69113][101321] = 123123;
+skinImage_List[69113][101322] = 123124;
+skinImage_List[69113][101323] = 123122;
+skinImage_List[69113][101324] = 123125;
+
+skinImage_List[69114] = {};
+skinImage_List[69114][101400] = 123115;
+skinImage_List[69114][101401] = 123117;
+skinImage_List[69114][101402] = 123116;
+skinImage_List[69114][101403] = 123119;
+skinImage_List[69114][101404] = 123118;
+skinImage_List[69114][101405] = 123120;
+
+skinImage_List[69115] = {};
+skinImage_List[69115][101410] = 123160;
+skinImage_List[69115][101411] = 123161;
+skinImage_List[69115][101412] = 123163;
+skinImage_List[69115][101413] = 123162;
+
+skinImage_List[69116] = {};
+skinImage_List[69116][101430] = 123134;
+skinImage_List[69116][101431] = 123133;
+skinImage_List[69116][101432] = 123132;
+skinImage_List[69116][101433] = 123135;
+skinImage_List[69116][101434] = 123131;
+
+skinImage_List[69117] = {};
+skinImage_List[69117][101500] = 123182;
+skinImage_List[69117][101501] = 123183;
+skinImage_List[69117][101502] = 123185;
+skinImage_List[69117][101503] = 123184;
+
+skinImage_List[69118] = {};
+skinImage_List[69118][101510] = 123178;
+skinImage_List[69118][101511] = 123181;
+skinImage_List[69118][101512] = 123180;
+skinImage_List[69118][101513] = 123179;
+
+skinImage_List[69119] = {};
+skinImage_List[69119][101520] = 123042;
+skinImage_List[69119][101521] = 123039;
+skinImage_List[69119][101522] = 123040;
+skinImage_List[69119][101523] = 123041;
+skinImage_List[69119][101524] = 123043;
+
+skinImage_List[69120] = {};
+skinImage_List[69120][101530] = 123171;
+skinImage_List[69120][101531] = 123172;
+skinImage_List[69120][101532] = 123170;
+skinImage_List[69120][101533] = 123173;
+
+skinImage_List[69121] = {};
+skinImage_List[69121][101600] = 123031;
+skinImage_List[69121][101601] = 123030;
+skinImage_List[69121][101602] = 123028;
+skinImage_List[69121][101603] = 123029;
+
+skinImage_List[69122] = {};
+skinImage_List[69122][101611] = 123145;
+skinImage_List[69122][101612] = 123144;
+skinImage_List[69122][101613] = 123142;
+skinImage_List[69122][101614] = 123143;
+skinImage_List[69122][101615] = 123146;
+
+skinImage_List[69123] = {};
+skinImage_List[69123][101620] = 123107;
+skinImage_List[69123][101621] = 123105;
+skinImage_List[69123][101622] = 123108;
+skinImage_List[69123][101623] = 123106;
+
+skinImage_List[69124] = {};
+skinImage_List[69124][101700] = 123158;
+skinImage_List[69124][101701] = 123156;
+skinImage_List[69124][101702] = 123157;
+skinImage_List[69124][101703] = 123159;
+
+skinImage_List[69125] = {};
+skinImage_List[69125][101710] = 123167;
+skinImage_List[69125][101711] = 123166;
+skinImage_List[69125][101712] = 123165;
+skinImage_List[69125][101713] = 123168;
+skinImage_List[69125][101714] = 123164;
+
+skinImage_List[69126] = {};
+skinImage_List[69126][101720] = 123081;
+skinImage_List[69126][101721] = 123082;
+skinImage_List[69126][101722] = 123080;
+skinImage_List[69126][101723] = 123083;
+
+skinImage_List[69127] = {};
+skinImage_List[69127][101800] = 123060;
+skinImage_List[69127][101801] = 123062;
+skinImage_List[69127][101802] = 123061;
+skinImage_List[69127][101803] = 123063;
+
+skinImage_List[69128] = {};
+skinImage_List[69128][101810] = 123076;
+skinImage_List[69128][101811] = 123077;
+skinImage_List[69128][101812] = 123078;
+skinImage_List[69128][101813] = 123079;
+
+skinImage_List[69129] = {};
+skinImage_List[69129][101820] = 123034;
+skinImage_List[69129][101821] = 123032;
+skinImage_List[69129][101822] = 123033;
+
+skinImage_List[69130] = {};
+skinImage_List[69130][101830] = 123095;
+skinImage_List[69130][101831] = 123098;
+skinImage_List[69130][101832] = 123097;
+skinImage_List[69130][101833] = 123099;
+skinImage_List[69130][101834] = 123096;
+
+skinImage_List[69131] = {};
+skinImage_List[69131][101920] = 123102;
+skinImage_List[69131][101921] = 123104;
+skinImage_List[69131][101922] = 123101;
+skinImage_List[69131][101923] = 123103;
+
+skinImage_List[69132] = {};
+skinImage_List[69132][101930] = 123128;
+skinImage_List[69132][101931] = 123130;
+skinImage_List[69132][101932] = 123127;
+skinImage_List[69132][101933] = 123126;
+skinImage_List[69132][101934] = 123129;
+
+skinImage_List[69133] = {};
+skinImage_List[69133][107700] = 123037;
+skinImage_List[69133][107701] = 123038;
+skinImage_List[69133][107702] = 123036;
+skinImage_List[69133][107703] = 123035;
+
+skinImage_List[69134] = {};
+skinImage_List[69134][101111] = 123194;
+skinImage_List[69134][101112] = 123195;
+skinImage_List[69134][123099] = 123192;
+skinImage_List[69134][123096] = 123193;
+skinImage_List[69134][123074] = 123199;
+skinImage_List[69134][123115] = 123191;
+skinImage_List[69134][123116] = 123190;
+skinImage_List[69134][123120] = 123189;
+skinImage_List[69134][123073] = 123198;
+skinImage_List[69134][123071] = 123196;
+skinImage_List[69134][123072] = 123197;
+-------------------------------------------------------------------------------------------------------------------------------------
+--- 加载模块钩子
+function Module:onLoad()
+  self:logInfo('load')
+  self:regCallback('ProtocolOnRecv',Func.bind(self.petEquipmentCommand,self),'Cpa')
+
+end
+
+function Module:petEquipmentCommand(fd, head, data)	--data2道具栏位置.data3宠物位置.data4宠物装备栏位置(Cpa 0 j 2)
+  local player = Protocol.GetCharByFd(fd)
+  print(player,data[2],data[3],data[4],data[5])
+  if (tonumber(data[3])>0) then		--装备接口
+    local petIndex = Char.GetPet(player,tonumber(data[3])-1);
+    local itemSlot = from62to10(data[2]);
+    local playerItemIndex = Char.GetItemIndex(player,tonumber(itemSlot));
+    local petItemIndex = Char.GetItemIndex(petIndex,tonumber(data[4]));
+    print(petIndex, playerItemIndex, petItemIndex)
+    local itemId = Item.GetData(playerItemIndex, CONST.道具_ID);
+    local type = Item.GetData(playerItemIndex, CONST.道具_类型);
+    setItemEffectData(petIndex, playerItemIndex, itemId, type);
+  elseif (tonumber(data[3])==0) then	--卸下接口
+    for petSlot=0,4 do
+      local petIndex = Char.GetPet(player,petSlot);
+      if (petIndex>0) then
+        local petItemIndex = Char.GetItemIndex(petIndex,tonumber(data[2]));
+        local itemSlot = from62to10(data[4]);
+        local playerItemIndex = Char.GetItemIndex(player,tonumber(itemSlot));
+        print(petIndex, playerItemIndex, petItemIndex)
+        if (petItemIndex>0) then
+          local itemId = Item.GetData(petItemIndex, CONST.道具_ID);
+          local type = Item.GetData(petItemIndex, CONST.道具_类型);
+          setItemReEffectData(petIndex, petItemIndex, itemId, type);
+        end
+      end
+    end
+  end
+end
+
+------------------------------------------------------------------------------------------
+--功能函数
+--装备时
+function setItemEffectData(_CharIndex, _ItemIndex, _ItemId, _Type)
+	if (_Type==58 or _Type==59) then	--CONST.道具类型_宠物装甲.CONST.道具类型_宠物服饰
+		local petImageId = Char.GetData(_CharIndex, CONST.对象_原形);		--CONST.对象_原始图档
+		local change_imageId = skinImage_List[_ItemId][petImageId] or 0;
+		print(petImageId,change_imageId)
+		if (change_imageId>0) then
+			print(petImageId)
+			Char.SetData(_CharIndex, CONST.对象_形象, change_imageId);
+			NLG.UpChar(_CharIndex);
+		elseif (change_imageId<=0) then
+			Char.SetData(_CharIndex, CONST.对象_形象, petImageId);
+			NLG.UpChar(_CharIndex);
+		end
+	end
+end
+
+--卸下时
+function setItemReEffectData(_CharIndex, _ItemIndex, _ItemId, _Type)
+	if (_Type==58 or _Type==59) then	--CONST.道具类型_宠物装甲.CONST.道具类型_宠物服饰
+		local petImageId = Char.GetData(_CharIndex, CONST.对象_原形);		--CONST.对象_原始图档
+		local change_imageId = skinImage_List[_ItemId][petImageId] or 0;
+		print(petImageId,change_imageId)
+		if (change_imageId>0) then
+			print(petImageId)
+			Char.SetData(_CharIndex, CONST.对象_形象, petImageId);
+			local player = Pet.GetOwner(_CharIndex);
+			Pet.UpPet(player, _CharIndex);
+			NLG.UpChar(player);
+		end
+	end
+end
+
+function from62to10(result)
+	local dict = {"0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+		"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};--进制数
+	local num = -1;
+	for i=1,62 do
+		if (tostring(dict[i])==result) then
+			num = i-1;
+			return num
+		end
+	end
+	return num
+end
+
+function from10to62(num)
+	local dict = {"0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+		"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};--进制数
+	local result = ''
+	--local bin = ''
+	while num > 0 do
+		result = tostring(dict[(num % 62)+1]) .. result--取余数并拼接到进制数的前面
+		num = math.floor(num / 62)--整除62
+	end
+	return result
+end
+
+--- 卸载模块钩子
+function Module:onUnload()
+  self:logInfo('unload')
+end
+
+return Module;
